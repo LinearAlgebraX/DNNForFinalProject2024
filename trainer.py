ed4d1e232bfb7b640fa43225bbd796ab1bf9b5f1
foma1haut <foma1haut@proton.me>
2023-10-10 23:28:52 +0800

    basic DNN accuracy: 75%

---
+++ trainer.py
@@ -0,0 +1,96 @@
+import torch
+import torchvision
+
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import cv2
+
+train_dataset = datasets.MNIST(root="data/", train=True, transform=transforms.ToTensor(), download=True)
+test_dataset = datasets.MNIST(root="data/", train=False, transform=transforms.ToTensor(), download=True)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=100, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=100, shuffle=True)
+
+# images, labels = next(iter(train_loader))
+# img = torchvision.utils.make_grid(images, nrow=10)
+# img = img.numpy().transpose(1, 2, 0)
+# cv2.imshow("img", img)
+# cv2.waitKey(0)
+
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "cpu")
+
+print(f"Using {device} device\n")
+
+
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = torch.nn.Flatten()
+        self.linear_relu_stack = torch.nn.Sequential(
+            torch.nn.Linear(28*28, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 10))
+        
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+    
+model = NeuralNetwork().to(device)
+
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 10
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-----------------------------------------")
+    train(train_loader, model, loss_fn, optimizer)
+    test(test_loader, model, loss_fn)
+print("Done!")
+
+
+Enter = input("Do you want to save this model?[y/n]")
+
+if Enter == "y":
+    torch.save(model.state_dict(), "model.pth")
+    print("Saved PyTorch Model State to model.pth")
